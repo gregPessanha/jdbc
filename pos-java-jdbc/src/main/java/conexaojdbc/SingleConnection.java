751cd3b7815aec0db3342cacac4eaaa9fd9d540f
gregPessanha <87192551+gregPessanha@users.noreply.github.com>
2021-09-16 21:28:55 -0300

    Indentação da classe singleConnection

--- pos-java-jdbc/src/main/java/conexaojdbc/SingleConnection.java
+++ pos-java-jdbc/src/main/java/conexaojdbc/SingleConnection.java
@@ -3,9 +3,9 @@ package conexaojdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class SingleConnection { /* CLASSE DE CONEXÃO COM O BANCO DE DADOS;  1º PASSO*/
+public class SingleConnection { /* CLASSE DE CONEXÃO COM O BANCO DE DADOS; 1º PASSO */
 
-	private static String url = "jdbc:postgresql://localhost:5432/posjava"; 
+	private static String url = "jdbc:postgresql://localhost:5432/posjava";
 	private static String user = "postgres";
 	private static String password = "admin";
 	private static Connection connection = null;
@@ -16,14 +16,14 @@ public class SingleConnection { /* CLASSE DE CONEXÃO COM O BANCO DE DADOS;  1º
 
 	private static void conectar() {
 		try {
-			
+
 			if (connection == null) { /* SE A CONEXÃO FOR NULA, OU SEJA, NÃO TIVER CONEXÃO, CONECTA */
 				Class.forName("org.postgresql.Driver"); /* CARREGAMENTO DO DRIVER QUE SERÁ USADO; */
 				connection = DriverManager.getConnection(url, user, password);
 				connection.setAutoCommit(false); /* NÃO SALVA AUTOMATICAMENTE; */
 				System.out.println("Conexão bem-Sucedida");
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -32,13 +32,8 @@ public class SingleConnection { /* CLASSE DE CONEXÃO COM O BANCO DE DADOS;  1º
 	public SingleConnection() { /* CONSTRUTOR QUE CHAMA O MÉTODO CONECTAR */
 		conectar();
 	}
-	
-	
+
 	public static Connection getConnection() {
 		return connection;
 	}
-	
-	
-	
-	
 }
